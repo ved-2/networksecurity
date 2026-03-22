4d92567f26416d40d467e38298184db62fe5a367
ved-2 <vedantkolte18@gmail.com>
2026-03-22 09:58:26 +0530

    setup.py ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements()->List[str]:
+  
+  requirement_lst:List[str]=[]
+  try:
+    with open('requirements.txt','r') as file:
+      lines=file.readlines()
+      
+      for line in lines:
+        requirement=line.strip()
+        
+        if requirement and requirement!='-e .':
+          requirement_lst.append(requirement)
+  except FileNotFoundError:
+    print("requirements.txt file not found")
+    
+  return requirement_lst
+
+setup(
+  name="Network Security",
+  version="0.0.1",
+  author="Vedant Kolte",
+  author_email="vedantkolte18@gmail.com",
+  packages=find_packages(),
+  install_requires=get_requirements()
+)

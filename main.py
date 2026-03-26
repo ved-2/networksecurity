da37e940bb20a70170162f20f84df532beb42a66
ved-2 <vedantkolte18@gmail.com>
2026-03-26 10:41:55 +0530

    data transformation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
 import sys
 from networksecurity.components.data_validation import DataValidationConfig,DataValidation
+from networksecurity.components.data_transformation import DataTransformation,DataTransformationConfig
 
 if __name__ == "__main__":
     try:
@@ -15,18 +16,27 @@ if __name__ == "__main__":
       
       logging.info("Initiate the data ingestion")
       dataingestionartifact=dataingestion.initiate_data_ingestion()
+      logging.info("Data ingestion completed")
       print(dataingestionartifact)
       
       
       data_validation_config=DataValidationConfig(trainingpipelineconfig)
       data_validation=DataValidation(dataingestionartifact,data_validation_config)
       
-      logging.info("Initiate the dat Validation")
+      logging.info("Initiate the data Validation")
       
       data_validation_artifact=data_validation.initiate_data_validation()
-      logging.info("Initiate the data validation")
+      logging.info("Data validation completed")
       
       print(data_validation_artifact)
+      
+      data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+      logging.info("Data transfromation started")
+      data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+
+      data_transformation_artifact=data_transformation.initiate_data_transformation()
+      logging.info("Data transfromation completed")
+      print(data_transformation_artifact)
 
 
     except Exception as e:
